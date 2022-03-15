96ef3013f050d5b6b806f193c4b0025b569b2a04
Behzod Mamadiev <bmamadiev@gmail.com>
2022-03-15 21:33:05 +0300

    Update 9

--- IntegrationTests/src/test/java/com/kenzie/unit/two/UserRoleCustomTest.java
+++ IntegrationTests/src/test/java/com/kenzie/unit/two/UserRoleCustomTest.java
@@ -8,6 +8,7 @@ public class UserRoleCustomTest {
         // TODO - write this test
         // Hint - Look at the test in missingUserRoleThrowsException_TASK_2.
         // This will demonstrate how to assert an exception has been thrown
+
         assert(false);
     }
 }
